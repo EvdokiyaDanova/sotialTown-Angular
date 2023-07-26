b9b0e7e87979cbff56033829f2d78afb63b3b7d8
EvdokiyaDanova <evdokiq.danova@gmail.com>
2023-07-26 20:54:45 +0300

    style login and register form

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -19,6 +19,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 
@@ -39,6 +40,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatMenuModule
   ],
   exports: [
     HeaderComponent,
