1fb63ea3f9abd685c9edb6c3acf2ebc6ac53dc89
EvdokiyaDanova <evdokiq.danova@gmail.com>
2024-06-28 15:16:08 +0300

    show segestion only for 1 input

--- client/src/app/feature/events/events-new-page/events-new-page.component.ts
+++ client/src/app/feature/events/events-new-page/events-new-page.component.ts
@@ -1,12 +1,13 @@
 /// <reference types="@types/google.maps" />
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, HostListener } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { mergeMap } from 'rxjs/operators';
-import { NgZone } from '@angular/core'; // Import NgZone
 import { EventService } from 'src/app/core/event.service';
 import { IEvent, IPost } from 'src/app/core/interfaces';
+
+declare var google: any;
 
 @Component({
   selector: 'app-events-new-page',
@@ -14,7 +15,6 @@ import { IEvent, IPost } from 'src/app/core/interfaces';
   styleUrls: ['./events-new-page.component.css']
 })
 export class EventsNewPageComponent implements OnInit {
-
   eventId: string | null;
   isEditPage: boolean = false;
   event: IEvent<IPost, string>;
@@ -36,23 +36,23 @@ export class EventsNewPageComponent implements OnInit {
 
   addressSuggestions: any[] = [];
   newEventForm: NgForm; // for Template Drivens Forms
+  activeField: string = ''; // Track which field is active
+  activeSuggestionIndex: number = -1; // Track active suggestion
+  showSuggestions: boolean = false; // Track if suggestions should be shown
 
   constructor(
     private router: Router,
     private eventService: EventService,
     private activatedRoute: ActivatedRoute,
     private ngZone: NgZone,
-  ) { } 
-    
+  ) { }
 
   ngOnInit(): void {
-    // get url params and check page
     this.activatedRoute.params.subscribe(params => {
       this.eventId = params['eventId'];
       this.isEditPage = !!this.eventId;
 
       if (this.isEditPage) {
-        console.log("isEditPage ", this.isEditPage);
         this.loadEvent(this.eventId);
       }
     });
@@ -77,7 +77,7 @@ export class EventsNewPageComponent implements OnInit {
       .pipe(
         mergeMap(params => {
           const eventId = params['eventId'];
-          return this.eventService.loadEventById(eventId)
+          return this.eventService.loadEventById(eventId);
         })
       )
       .subscribe((event) => {
@@ -96,49 +96,44 @@ export class EventsNewPageComponent implements OnInit {
         this.eventDescription = event.eventDescription;
         this.eventStaticPhoto = event.eventStaticPhoto;
         this.eventVideoUrl = event.eventVideoUrl;
-
-        console.log("event ", this.event);
-      })
+      });
   }
 
   createEvent(newEventForm: any) {
-    console.log("newEventForm.value");
-    console.log(newEventForm.value);
     this.eventService.addEvent$(newEventForm.value).subscribe({
       next: (event) => {
-        console.log(event);
         this.router.navigate(['/events']);
       },
       error: (error) => {
         console.error(error);
       }
-    })
+    });
   }
 
   updateEvent(eventId: string, newEventForm: any) {
-    console.log("newEventForm.value");
-    console.log(newEventForm.value);
-    const eventName = newEventForm.value.eventName;
-    console.log("uodate for is edited");
     this.eventService.editEvent(eventId, newEventForm.value).subscribe({
       next: (event) => {
-        console.log(newEventForm.value);
         this.router.navigate(['/events']);
       },
       error: (error) => {
         console.error(error);
       }
-    })
+    });
   }
 
-  // Method to get address suggestions
-  getSuggestions(query: string) {
+  getSuggestions(query: string, field: string) {
+    this.activeField = field;
     if (query.length > 2) {
       this.eventService.getPlacePredictions(query).subscribe(suggestions => {
-        this.addressSuggestions = suggestions;
+        this.ngZone.run(() => {
+          this.addressSuggestions = suggestions;
+          this.activeSuggestionIndex = -1; // Reset the active suggestion index
+          this.showSuggestions = true;
+        });
       });
     } else {
       this.addressSuggestions = [];
+      this.showSuggestions = false;
     }
   }
 
@@ -147,12 +142,55 @@ export class EventsNewPageComponent implements OnInit {
     placeService.getDetails({ placeId: suggestion.place_id }, (place, status) => {
       this.ngZone.run(() => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-          this.eventCountry = place.address_components.find(component => component.types.includes('country'))?.long_name || '';
-          this.eventCity = place.address_components.find(component => component.types.includes('locality'))?.long_name || '';
-          this.eventAddress = place.formatted_address;
+          if (this.activeField === 'eventCountry') {
+            this.eventCountry = place.address_components.find(component => component.types.includes('country'))?.long_name || '';
+          } else if (this.activeField === 'eventCity') {
+            this.eventCity = place.address_components.find(component => component.types.includes('locality'))?.long_name || '';
+          } else if (this.activeField === 'eventAddress') {
+            this.eventAddress = place.formatted_address;
+          }
           this.addressSuggestions = [];
+          this.showSuggestions = false;
         }
       });
     });
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (this.showSuggestions && this.addressSuggestions.length > 0) {
+      switch (event.key) {
+        case 'ArrowDown':
+          event.preventDefault();
+          this.activeSuggestionIndex = (this.activeSuggestionIndex + 1) % this.addressSuggestions.length;
+          break;
+        case 'ArrowUp':
+          event.preventDefault();
+          this.activeSuggestionIndex = (this.activeSuggestionIndex - 1 + this.addressSuggestions.length) % this.addressSuggestions.length;
+          break;
+        case 'Enter':
+          if (this.activeSuggestionIndex >= 0 && this.activeSuggestionIndex < this.addressSuggestions.length) {
+            this.selectSuggestion(this.addressSuggestions[this.activeSuggestionIndex]);
+            event.preventDefault();
+          }
+          break;
+        case 'Escape':
+          this.showSuggestions = false;
+          break;
+      }
+    }
+  }
+
+  hideSuggestions() {
+    setTimeout(() => {
+      this.showSuggestions = false;
+    }, 200);
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.suggestions')) {
+      this.showSuggestions = false;
+    }
   }
 }
